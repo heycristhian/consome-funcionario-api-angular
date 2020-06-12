b13f51daf320a1a590a5e5926ce0433e68b1283f
heycristhian <heycristhian@gmail.com>
2020-06-12 16:12:20 -0300

    arrumado url func form service

--- src/app/funcionario/register-funcionario/register-funcionario.service.ts
+++ src/app/funcionario/register-funcionario/register-funcionario.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-const url = 'http://localhost:8080/heycristhian-service/funcionarios';
+const url = 'http://localhost:8080/service/funcionarios';
 
 @Injectable({
   providedIn: 'root'
